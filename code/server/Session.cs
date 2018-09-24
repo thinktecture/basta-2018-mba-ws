b8a4104bdfe5008a34d599ff3577957847496c33
Christian Weyer <christian.weyer@thinktecture.com>
2018-09-24 11:28:32 +0200

    Initial server-less code

---
+++ code/server/Session.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Session
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Abstract { get; set; }
+    public Speaker Speaker { get; set; }
+}
---
+++ code/server/SessionsDetails.cs
@@ -0,0 +1,31 @@
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Serverless
+{
+    public static class SessionsDetails
+    {
+        [FunctionName("SessionsDetails")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "sessions/{id}")]
+            HttpRequest req,
+            [CosmosDB("conferences", "data", ConnectionStringSetting="CosmosDB", Id="{id}")]
+            Session session,
+            ILogger log)
+        {
+            // TODO: handle 404 :-)
+            // TODO: handle exceptions
+
+            return new OkObjectResult(session);
+        }
+    }
+}
